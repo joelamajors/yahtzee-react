3f3b3f72c255c41d735257fad986d23038863e7d
joelamajors <joelamajors@gmail.com>
2018-02-17 11:33:50 -0500

    abstracted header

---
+++ yahtzee-react/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../logo.svg'
+// import { NavLink } from 'react-router-dom'
+
+const Header = () =>
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Welcome to React</h1>
+  </header>
+
+export default Header
